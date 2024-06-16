40ec98961daddccbddb666e55c6eeb030483cb00
Semen Solovyov <semsolovyov@yandex.ru>
2024-06-16 18:12:15 +0500

    working

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import datetime
 from flask import Flask, abort, make_response, redirect, render_template, request, session, url_for
 from data import db_session
 from data.category import Category
@@ -12,7 +11,7 @@ from flask_login import LoginManager, current_user, login_required, login_user, 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
-db_session.global_init("db/blogs.sqlite") # добавляем вызов глобальной инициализации всего, что связано с БД
+db_session.global_init("db/blogs.sqlite") 
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -49,7 +48,7 @@ def cookie_test():
         res.set_cookie("visits_count", '1', max_age=60 * 60 * 24 * 365 * 2)
     else:
         res = make_response("Вы пришли на эту страницу в первый раз за последние 2 года")
-        res.set_cookie("visits_count", '1',max_age=60 * 60 * 24 * 365 * 2)
+        res.set_cookie("visits_count", '1', max_age=60 * 60 * 24 * 365 * 2)
     
     return res
 
@@ -64,7 +63,7 @@ def reqister():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',form=form,message="Пароли не совпадают")
+            return render_template('register.html', title='Регистрация', form=form, message="Пароли не совпадают")
         db_sess = db_session.create_session()
        
         user = db_sess.query(User).filter(User.email == form.email.data).first()
@@ -73,9 +72,9 @@ def reqister():
             return render_template('register.html', title='Регистрация', form=form, message="Такой пользователь уже есть")
         
         user = User(
-        name=form.name.data,
-        email=form.email.data,
-        about=form.about.data
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
         )
 
         user.set_password(form.password.data)
@@ -94,13 +93,10 @@ def login():
 
         user = db_sess.query(User).filter(User.email == form.email.data).first()
 
-        # Проверяем, существует ли пользователь и пароль верен
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
-            # Успешная аутентификация
-            return redirect('/') # Перенаправление на страницу после входа
+            return redirect('/')
         else:
-            # Неправильный логин или пароль
             error = 'Invalid username or password'
             return render_template('login.html', error=error)
 
@@ -116,9 +112,9 @@ def add_news():
             title=form.title.data,
             content=form.content.data,
             is_private=form.is_private.data,
-            user_id=current_user.id,
-            categories=[db_sess.query(Category).get(id) for id in form.categories.data]
+            user_id=current_user.id
         )
+        news.categories = [db_sess.query(Category).get(id) for id in form.categories.data]
         db_sess.add(news)
         db_sess.commit()
         return redirect('/')
@@ -152,6 +148,7 @@ def edit_news(id):
             abort(404)
     return render_template('news.html', title='Редактирование новости', form=form)
 
+
 @app.route('/news_delete/<int:id>', methods=['GET', 'POST'])
 @login_required
 def news_delete(id):
@@ -176,86 +173,5 @@ def add_category():
         return redirect('/')
     return render_template('category.html', title='Добавление категории', form=form)
 
-def main():
-    db_sess = db_session.create_session()
-
-    user = User()
-    # user.name = "Пользователь 1"
-    # user.about = "биография пользователя 1"
-    # user.email = "email@email1.ru"
-    # db_sess.add(user)
-    # db_sess.commit()
-
-    # user = User()
-    # user.name = "Пользователь 2"
-    # user.about = "биография пользователя 2"
-    # user.email = "email@email2.ru"
-    # db_sess.add(user)
-    # db_sess.commit()
-
-    # user = User()
-    # user.name = "Пользователь 3"
-    # user.about = "биография пользователя 3"
-    # user.email = "email@email3ru"
-    # db_sess.add(user)
-    # db_sess.commit()
-
-    # выбор первого пользователя
-    user = db_sess.query(User).first()
-    print(user.name)
-
-    # вывод всех пользователей
-    for user in db_sess.query(User).all():
-        print(user)
-
-    # вывод пользователей по условию используя AND
-    for user in db_sess.query(User).filter(User.id > 1, User.email.notilike("%1%")):
-        print(user.name)
-
-    # вывод пользователей по условию используя OR
-    for user in db_sess.query(User).filter((User.id > 1) | (User.email.notilike("%1%"))):
-        print(user.name)
-
-    # Изменение пользователя
-    # user = db_sess.query(User).filter(User.id == 1).first()
-    # print(user.name)
-    # user.name = "Измененное имя пользователя"
-    # user.created_date = datetime.datetime.now()
-    # db_sess.commit()
-
-    # Удаление пользователей по фильтру
-    # db_sess.query(User).filter(User.id >= 3).delete()
-    # db_sess.commit()
-
-    # Удаление пользователя по ID
-    # user = db_sess.query(User).filter(User.id == 2).first()
-    # db_sess.delete(user)
-    # db_sess.commit()
-
-    # Добавление новости для пользователя с id 1
-    # news = News(title="Первая новость", content="Привет блог!",
-    # user_id=1, is_private=False)
-    # db_sess.add(news)
-    # db_sess.commit()
-
-    # Добавление новости через объект класса
-    # user = db_sess.query(User).filter(User.id == 1).first()
-    # news = News(title="Вторая новость", content="Уже вторая запись!",
-    # user=user, is_private=False)
-    # db_sess.add(news)
-    # db_sess.commit()
-    
-    # Добавление новости через список
-    # user = db_sess.query(User).filter(User.id == 1).first()
-    # news = News(title="Личная запись", content="Эта запись личная",
-    # is_private=True)
-    # user.news.append(news)
-    # db_sess.commit()
-
-    # Просмотр новостей пользователя
-    for news in user.news:
-        print(news)
-
 if __name__ == '__main__':
     app.run(port=700, host='127.0.0.1')
-    #main()
